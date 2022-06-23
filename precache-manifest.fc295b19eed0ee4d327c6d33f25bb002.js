77669e29d12831f1fa0d8265b568cb813bca5673
Suciana Dewi <sucianadewwi@gmail.com>
2022-06-23 15:52:28 +0800

    Updates

---
+++ precache-manifest.fc295b19eed0ee4d327c6d33f25bb002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0abb36ef5beb3761c463",
+    "url": "/todo-list-reactJS/static/css/main.edaa662a.chunk.css"
+  },
+  {
+    "revision": "0abb36ef5beb3761c463",
+    "url": "/todo-list-reactJS/static/js/main.0abb36ef.chunk.js"
+  },
+  {
+    "revision": "c197ac099bfd4f8f2725",
+    "url": "/todo-list-reactJS/static/js/1.c197ac09.chunk.js"
+  },
+  {
+    "revision": "6f998f8123c91d828c4a",
+    "url": "/todo-list-reactJS/static/js/runtime~main.6f998f81.js"
+  },
+  {
+    "revision": "124936c8aafdddb04c1f6e296ab852e3",
+    "url": "/todo-list-reactJS/index.html"
+  }
+];
